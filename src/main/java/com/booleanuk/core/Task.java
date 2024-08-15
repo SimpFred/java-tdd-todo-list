1ca26184115200de81156323557fec491d43d283
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-15 09:42:53 +0200

    Refactored instance variables to private

--- src/main/java/com/booleanuk/core/Task.java
+++ src/main/java/com/booleanuk/core/Task.java
@@ -1,4 +1,12 @@
 package com.booleanuk.core;
 
 public class Task {
+
+    private String name;
+    private   String description;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
