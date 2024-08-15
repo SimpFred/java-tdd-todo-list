9f2277e75eb6ee29719a4537bbc008f9914425ce
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-15 09:43:21 +0200

    TestGetName() fail

--- src/test/java/com/booleanuk/core/TaskTest.java
+++ src/test/java/com/booleanuk/core/TaskTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
-    
+
     @Test
     public void testCreatingATask() {
         Task todoList = new Task("task name", "task description");
         Assertions.assertNotNull(todoList, "The TodoList object should not be null after creation");
+    }
+
+    @Test
+    public void testGetName() {
+        Task todoList = new Task("task name", "task description");
+        Assertions.assertEquals("task name", todoList.getName(), "The name of the task should be 'task name'");
     }
 }
