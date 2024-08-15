773c62d1ef17d4be25019526cc60a418acd50f7e
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-15 09:44:20 +0200

    Test pass

--- src/main/java/com/booleanuk/core/Task.java
+++ src/main/java/com/booleanuk/core/Task.java
@@ -2,11 +2,15 @@ package com.booleanuk.core;
 
 public class Task {
 
-    private String name;
-    private   String description;
+    private final String name;
+    private final String description;
 
     public Task(String name, String description) {
         this.name = name;
         this.description = description;
+    }
+
+    public String getName() {
+        return name;
     }
 }
