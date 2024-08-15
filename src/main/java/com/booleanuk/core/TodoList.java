d5b6a69f18d266546d103dba4d29d1ceda6eda7e
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-15 10:17:53 +0200

    Refactored addTask

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,8 +1,20 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TodoList {
 
+    private Map<Task, Boolean> tasks;
+
+    public TodoList() {
+        this.tasks = new HashMap<>();
+    }
     public boolean addTask(Task task, boolean isCompleted) {
+        if (task == null) {
+            return false;
+        }
+        tasks.put(task, isCompleted);
         return true;
     }
 }
